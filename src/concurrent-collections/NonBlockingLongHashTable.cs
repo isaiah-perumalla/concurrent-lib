8cd02b9d1532e315d5429b4bf7a433cab267a4be
isaiah-perumalla <cappadonza@hotmail.com>
2012-03-15 21:57:56 +0000

    first step in making hashtable non blocking and thread safe

--- src/concurrent-collections/NonBlockingLongHashTable.cs
+++ src/concurrent-collections/NonBlockingLongHashTable.cs
@@ -1,27 +1,32 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 
 namespace concurrent_collections
 {
-    public class NonBlockingLongHashTable<T> where T: class 
+    public class NonBlockingLongHashTable<T> where T : class
     {
         private readonly long[] _keys;
         private readonly object[] _values;
         private readonly uint _tableLength;
         private static readonly object EMPTY = new object();
         private int size;
+        private AtomicInt slots = new AtomicInt();
+        private static readonly object NO_MATCH_OLD = new object();
+        private static readonly object MATCH_ANY = new object();
+        private AtomicInt _size = new AtomicInt();
         private const long NO_KEY = 0;
 
         public NonBlockingLongHashTable(uint capacity)
         {
             _tableLength = PowerOf2ClosestTo(capacity*2);
             _keys = new long[_tableLength];
-             _values = new object[_tableLength];
+            _values = new object[_tableLength];
         }
 
-      
-        
+
+
         private static uint PowerOf2ClosestTo(uint num)
         {
             var n = num > 0 ? num - 1 : 0;
@@ -48,27 +53,23 @@ namespace concurrent_collections
         /// <param name="val1"></param>
         /// <returns></returns>
         public T Put(long key, T val1)
-        {   var index = ClaimSlotFor(key);
-                var previousVal = _values[index];
-                _values[index] = val1;
-                if (previousVal == EMPTY || previousVal == null)
-                {
-                    size++;
-                    return null;
-                }
-                return (T) previousVal;
-            
+        {
+            return PutIfMatch(key, val1, NO_MATCH_OLD);
         }
 
-        private long ClaimSlotFor(long key)
+        private long ClaimSlotFor(long key, out object previousVal)
         {
             var index = IndexOf(key);
             while (true)
             {
+                previousVal = _values[index];
                 if (_keys[index] == NO_KEY)
                 {
-                    _keys[index] = key;
-                    break;
+                    if (CAS_key(index, NO_KEY, key))
+                    {
+                        slots.Increment();
+                        break;
+                    }
                 }
                 if (_keys[index] == key) break;
 
@@ -77,88 +78,103 @@ namespace concurrent_collections
             return index;
         }
 
+        private bool CAS_key(uint index, long compare, long val)
+        {
+            return Interlocked.CompareExchange(ref _keys[index], val, compare) == compare;
+        }
+
         public T PutIfAbsent(long key, Func<T> factoryFunction)
         {
-          
-                var index = ClaimSlotFor(key);
-                var previousVal = _values[index];
-                if (previousVal == EMPTY || previousVal == null)
+            object previousVal;
+            var index = ClaimSlotFor(key, out previousVal);
+            if (previousVal == EMPTY || previousVal == null)
+            {
+                return PutIfMatch(key, factoryFunction(), EMPTY);
+            }
+            return  previousVal as T;
+
+        }
+
+
+        private T PutIfMatch(long key, object putVal, object expectedVal)
+        {
+            if (putVal == null) throw new ArgumentException("null values cannot be inserted");
+            if (key <= 0) throw new ArgumentException("invalid key, key have to be greater than zero");
+
+            if (_keys[IndexOf(key)] == NO_KEY && putVal == EMPTY) return putVal as T;
+            object previousVal;
+            var idx = ClaimSlotFor(key, out previousVal);
+            while (true)
+            {
+                if (CannotMatchExpectedVal(expectedVal, previousVal)) return previousVal as T;
+                var casValue = CAS_Value(idx, previousVal, putVal);
+                if(casValue == previousVal)
                 {
-                    _values[index] = factoryFunction();
-                    size++;
-                    return null;
-                }
-                return (T) previousVal;
-            
+                    if ((previousVal == null || previousVal == EMPTY) && putVal != EMPTY) _size.Increment();
+                    if (!(previousVal == null || previousVal == EMPTY) && putVal == EMPTY) _size.Decrement();
+                }   
+                //cas failed get previous value
+                previousVal = _values[idx];
+            }
+
+
+        }
+
+        private object CAS_Value(long idx, object previousVal, object putVal)
+        {
+            return Interlocked.CompareExchange(ref _values[idx], putVal, previousVal);
+        }
+
+        private static bool CannotMatchExpectedVal(object expectedVal, object previousVal)
+        {
+            return expectedVal != NO_MATCH_OLD && // Do we care about expected-Value at all?
+                   previousVal != expectedVal && // No instant match already?
+                   (expectedVal != MATCH_ANY || previousVal == EMPTY || previousVal == null) &&
+                   previousVal != expectedVal &&
+                   !(previousVal == null && expectedVal == EMPTY);
         }
 
         public T PutIfAbsent(long key, T val)
         {
-            
-
-                var index = ClaimSlotFor(key);
-                var previousVal = _values[index];
-                if (previousVal == EMPTY || previousVal == null)
-                {
-                    _values[index] = val;
-                    size++;
-                    return (T)null;
-                }
-                return (T) previousVal;
-            
+            return PutIfMatch(key, val, EMPTY);
         }
 
         public T Remove(long key)
         {
-            
+            return PutIfMatch(key, EMPTY, NO_MATCH_OLD);
 
-                var index = IndexOf(key);
-                while (true)
-                {
-                    if (_keys[index] == NO_KEY) return null;
-                    if (_keys[index] == key)
-                    {
-                        var v = _values[index];
-                        if(v == EMPTY) return null;
-                        var currentVal = (T) v;
-                        _values[index] = EMPTY;
-                        size--;
-                        return currentVal;
-                    }
-                    index = Next(index);
-                }
-            
         }
 
         public bool TryGetValue(long key, out T val)
         {
-            
 
-                val = null;
-                var index = IndexOf(key);
-                while (true)
+
+            val = null;
+            var index = IndexOf(key);
+            while (true)
+            {
+                if (_keys[index] == NO_KEY) return false;
+                var value = _values[index];
+                if (_keys[index] == key && value != EMPTY && value != null)
                 {
-                    if (_keys[index] == NO_KEY) return false;
-                    var value = _values[index];
-                    if (_keys[index] == key && value != EMPTY && value != null)
-                    {
-                        val = (T) value;
-                        return true;
-                    }
-                    index = Next(index);
-
+                    val = (T) value;
+                    return true;
                 }
-            
+                index = Next(index);
+
+            }
+
         }
 
         private uint Next(uint index)
         {
-            return (index + 1) & (_tableLength-1);
+            return (index + 1) & (_tableLength - 1);
         }
 
         private uint IndexOf(long key)
         {
-            return (uint)key&(_tableLength-1);
+            return (uint) key & (_tableLength - 1);
+            
         }
 
 
@@ -166,6 +182,19 @@ namespace concurrent_collections
         {
             T unused;
             return _keys.Where(k => TryGetValue(k, out unused)).ToArray();
+        }
+    }
+
+    public class AtomicInt
+    {
+        public void Increment()
+        {
+
+        }
+
+        public void Decrement()
+        {
+            
         }
     }
 }
